84f28b94c3eb6bbc31c44a425e085800c20f6ff5
Jonah Braun <Jonah@goodcoffee.ca>
2014-12-17 12:04:43 -0800

    added additional format. changed book ordering from alphabetical to wlc.

--- lemmize.rb
+++ lemmize.rb
@@ -1,23 +1,45 @@
 #!/usr/bin/env ruby
 
+###
+# Usage: comment or uncomment (add/remove #) from the begining of print
+# lines to customize what kind of file to output.
+# Save output with redirection, eg: ./lemmize.rb > tanach.txt
+###
+
 require 'rexml/document'
 
-Dir.glob('wlc/*xml') do |file|
+# WLC ordering
+books = ['Gen','Exod','Lev','Num','Deut',
+				 'Josh','Judg','1Sam','2Sam','1Kgs','2Kgs','Isa','Jer','Ezek',
+				 'Hos','Joel','Amos','Obad','Jonah','Mic',
+				 'Nah','Hab','Zeph','Hag','Zech','Mal',
+				 'Ps','Prov','Job','Song','Ruth','Lam',
+				 'Eccl','Esth','Dan','Ezra','Neh','1Chr','2Chr']
 
-	doc = REXML::Document.new IO.read(file)
+books.each do |file|
+
+	doc = REXML::Document.new IO.read('wlc/'+file+'.xml')
 
 	doc.elements.each('osis/osisText/div/chapter/verse') do |verse|
 		osis = verse.attribute('osisID')
 
-		print osis
+		# At the verse level
+		#print osis, ' '
 
 		verse.elements.each_with_index do |ele, i|
 			type = ele.name
 			if type=='w' then
-				lemma =   ele.attributes['lemma'] 
-				print " "+lemma
+				lemma = ele.attributes['lemma'].gsub(/\s+/, "")
+				word = ele.text.gsub('/', '').gsub(/\s+/, "")
+
+				# At the word leve.gsub(/\s+/, "")
+				#print lemma, ':', word, ' '
+				#print lemma, " "
+				print osis, " ", lemma, " ", word, "\n"
 			end
 		end
-		print "\n"
+		
+		# end of verse level
+		#print "\n"
 	end
 end
